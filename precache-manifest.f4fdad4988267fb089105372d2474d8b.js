fc8c8b13f351373010065546886555ddce679341
solomonbarayev <sbarayev2@gmail.com>
2022-07-18 11:46:25 +0300

    Updates

---
+++ precache-manifest.f4fdad4988267fb089105372d2474d8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ebf11305512369845e236e05f4e7f6",
+    "url": "/react-testimonials-component/index.html"
+  },
+  {
+    "revision": "ed11c9898863253f9ff9",
+    "url": "/react-testimonials-component/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/react-testimonials-component/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-testimonials-component/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed11c9898863253f9ff9",
+    "url": "/react-testimonials-component/static/js/main.90eb8163.chunk.js"
+  },
+  {
+    "revision": "5d549b1ac037ca78f472",
+    "url": "/react-testimonials-component/static/js/runtime-main.a21aefef.js"
+  }
+]);
